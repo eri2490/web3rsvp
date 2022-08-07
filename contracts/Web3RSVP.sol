4087538448b2b6a98423297b6f7bb194c0eee8e1
eri2490 <e.147xxx@gmail.com>
2022-08-07 12:45:02 +0900

    feat: create function for create new event

--- contracts/Web3RSVP.sol
+++ contracts/Web3RSVP.sol
@@ -16,4 +16,47 @@ contract Web3RSVP {
 
     mapping(bytes32 => CreateEvent) public idToEvent;
 
+    // when a user creates a new event, this will get called.
+    // have to handle
+    // - a uniqe ID
+    // - a reference to who created the event (a wallet address of the creator)
+    // - the time of the event (so we know when refunds should become available)
+    // - the maximum capacity of attendees for that event
+    // - the deposit amount for that event
+    // - keep track of those who RSVP'd
+    // - keep track of users who check into the event
+    function createNewEvent(
+        uint256 eventTimestamp,
+        uint256 deposit,
+        uint256 maxCapacity,
+        string calldata eventDataCID
+    ) external {
+        // generate an eventID based on other things passed in to generate a hash
+        bytes32 eventId = keccak256(
+            abi.encodePacked(
+                msg.sender,
+                address(this),
+                eventTimestamp,
+                deposit,
+                maxCapacity
+            )
+        );
+
+        address[] memory confirmedRSVPs;
+        address[] memory claimedRSVPs;
+
+        // this creates a new CreateEvent struct and adds it to the idToEvent mapping
+        idToEvent[eventId] = CreateEvent(
+            eventId,
+            eventDataCID,
+            msg.sender,
+            eventTimestamp,
+            deposit,
+            maxCapacity,
+            confirmedRSVPs,
+            claimedRSVPs,
+            false
+        );
+    }
+
 }
